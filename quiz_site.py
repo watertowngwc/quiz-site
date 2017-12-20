f13e3e83a6960f92fc7856480ee580c345bd52f5
Ubuntu <ubuntu@ip-172-31-9-63.ec2.internal>
2017-12-20 03:24:30 +0000

    Added a new quiz

--- quiz_site.py
+++ quiz_site.py
@@ -9,7 +9,7 @@ quiz_dir = 'quizzes'
 
 quizzes = {}
 for quiz in os.listdir(quiz_dir):
-    print 'Loading', quiz
+    print('Loading {}'.format(quiz))
     quizzes[quiz] = json.loads(open(os.path.join(quiz_dir, quiz)).read())
 
 @app.route('/')
@@ -21,36 +21,49 @@ def quiz(id):
     if id not in quizzes:
         return flask.abort(404)
     quiz = copy.deepcopy(quizzes[id])
-    questions = list(enumerate(quiz["questions"]))
-    random.shuffle(questions)
-    quiz["questions"] = map(lambda t: t[1], questions)
-    ordering = map(lambda t: t[0], questions)
+    # questions = list(enumerate(quiz["questions"]))
+    # quiz["questions"] = map(lambda t: t[1], questions)
+    # ordering = list(map(lambda t: t[0], questions))
 
-    return flask.render_template('quiz.html', id=id, quiz=quiz, quiz_ordering=json.dumps(ordering))
+    return flask.render_template('quiz.html', id=id, quiz=quiz)
 
 @app.route('/check_quiz/<id>', methods=['POST'])
 def check_quiz(id):
-    ordering = json.loads(flask.request.form['ord'])
     quiz = copy.deepcopy(quizzes[id])
-    print flask.request.form
-    quiz['questions'] = sorted(quiz['questions'], key=lambda q: ordering.index(quiz['questions'].index(q)))
-    print quiz['questions']
-    answers = dict( (int(k), quiz['questions'][int(k)]['options'][int(v)]) for k, v in flask.request.form.items() if k != 'ord' )
 
-    print answers
+    # create dict of the user's answers, with those answer details from the 
+    #   official quiz dict, which includes whether they are correct
+    answers = {}
+    for k, v in flask.request.form.items():
+        answers[int(k)] = quiz['questions'][int(k)]['options'][int(v)]
 
+    # debug prints
+    print(flask.request.form)
+    print(quiz['questions'])
+    print("answers dict:")
+    print(answers)
+    
+    # redirect to the quiz page if no questions were answered
     if not len(answers.keys()):
         return flask.redirect(flask.url_for('quiz', id=id))
 
-    for k in xrange(len(ordering)):
+    # if a question was un-answered, set as incorrect
+    for k in range(len(quiz['questions'])):
         if k not in answers:
             answers[k] = [None, False]
 
+    # answers list is created as a list of lists, each list containing the answer 
+    #   and a bool  for correct option/incorrect option
     answers_list = [ answers[k] for k in sorted(answers.keys()) ]
-    number_correct = len(filter(lambda t: t[1], answers_list))
+    number_correct = len([ a for a in answers_list if a[1] == True])
 
-    return flask.render_template('check_quiz.html', quiz=quiz, question_answer=zip(quiz['questions'], answers_list), correct=number_correct, total=len(answers_list))
+    return flask.render_template('check_quiz.html', 
+                                 quiz=quiz, 
+                                 question_answer=zip(quiz['questions'], answers_list), 
+                                 correct=number_correct, 
+                                 total=len(answers_list)
+                                 )
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
