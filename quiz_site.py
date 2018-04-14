6ca680b324ce5242d29a75831a15c1ef40d7bd29
Rob <s>
2018-04-14 17:42:25 -0400

    Changed answer-checking logic

--- quiz_site.py
+++ quiz_site.py
@@ -12,18 +12,18 @@ quizzes = {}
 for quiz in os.listdir(quiz_dir):
     print('Loading {}'.format(quiz))
     quizzes[quiz] = json.loads(open(os.path.join(quiz_dir, quiz)).read())
-# no
+
 style_button = json.loads(open(os.path.join('config', 'config.json')).read())
 
 @app.route('/')
 def index():
     return flask.render_template('index.html', quiz_names=zip(quizzes.keys(), map(lambda q: q['name'], quizzes.values())), style_button=style_button)
-# no
+
 @app.route('/highscore')
 def highscore():
     scoreboard= [["Maeve",235634],["Rebecca", 784356789]]# no
     return flask.render_template('highscore.html',scoreboard=scoreboard)
-# no
+
 @app.route('/about')
 def about():
     return flask.render_template('about.html',)
@@ -33,7 +33,6 @@ def change_style():
     global style_button
     if style_button == True:
         style_button = False
-        # no
     else:
         style_button = True
     return flask.redirect(flask.url_for('index'))
@@ -61,7 +60,7 @@ def fun():
 def search():
     print(flask.request.form["search"])
     all_quizzes = dict (zip(quizzes.keys(), map(lambda q: q['name'], quizzes.values())))
-    print(all_quizzes)# no
+    print(all_quizzes)
     print(type(all_quizzes))
     search_results = {}
     for idno in all_quizzes:
@@ -69,43 +68,54 @@ def search():
             search_results[idno] = all_quizzes[idno]
             print(search_results)
     return flask.render_template('search.html',quiz_names=search_results)
-# no
+
 @app.route('/check_quiz/<id>', methods=['POST'])
 def check_quiz(id):
+    responses = dict(flask.request.form.items())
+
+    # redirect to the quiz page if no questions were answered
+    if len(responses) == 0:
+        return flask.redirect(flask.url_for('quiz', id=id))
+
     quiz = copy.deepcopy(quizzes[id])
 
-    # create dict of the user's answers, with those answer details from the 
-    #   official quiz dict, which includes whether they are correct
-    answers = {}
-    for k, v in flask.request.form.items():
-        answers[int(k)] = quiz['questions'][int(k)]['options'][int(v)]
+    number_correct = 0
+    question_number = 0
+    for question in quiz['questions']:
+
+        question_correct = False
+        option_number = 0
+        for option in question['options']:
+
+            if str(question_number) in responses and responses[str(question_number)] == str(option_number):
+                option.append(True)
+
+                if option[1] == True:
+                    question_correct = True
+                    number_correct = number_correct + 1
+
+            else:
+                option.append(False)
+
+            option_number = option_number + 1
+
+        if question_correct == True:
+            question['is_correct'] = True
+        else:
+            question['is_correct'] = False
+
+        question_number = question_number + 1
 
     # debug prints
     print(flask.request.form)
     print(quiz['questions'])
-    print("answers dict:")
-    print(answers)
-    
-    # redirect to the quiz page if no questions were answered
-    if not len(answers.keys()):
-        return flask.redirect(flask.url_for('quiz', id=id))
-
-    # if a question was un-answered, set as incorrect
-    for k in range(len(quiz['questions'])):
-        if k not in answers:
-            answers[k] = [None, False]
-
-    # answers list is created as a list of lists, each list containing the answer 
-    #   and a bool  for correct option/incorrect option
-    answers_list = [ answers[k] for k in sorted(answers.keys()) ]
-    number_correct = len([ a for a in answers_list if a[1] == True])
+    print(responses)
 
     return flask.render_template('check_quiz.html', 
                                  quiz=quiz, 
-                                 question_answer=zip(quiz['questions'], answers_list), 
-                                 correct=number_correct, 
-                                 total=len(answers_list)
-                                 )# no
+                                 correct=number_correct,
+                                 total=len(quiz['questions'])
+                                 )
 
 
 if __name__ == '__main__':
